f8cebf5611e5fa588d6b479532f34ad4fdbb25e4
liwang-pivotal <liwang@pivotal.io>
2018-10-22 13:55:13 -0700

    integrated with adyen endpoint

--- pcc-inline-caching-client/src/main/java/io/pivotal/controller/ItemController.java
+++ pcc-inline-caching-client/src/main/java/io/pivotal/controller/ItemController.java
@@ -1,9 +1,20 @@
 package io.pivotal.controller;
 
+import java.io.IOException;
+
 import org.apache.geode.cache.Region;
 import org.apache.geode.pdx.JSONFormatter;
 import org.apache.geode.pdx.PdxInstance;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,6 +27,9 @@ public class ItemController {
 
 	@Autowired
 	Region<String, PdxInstance> itemRegion;
+	
+	@Value("${app.adyen.url}")
+	private String url;
 	
 	@RequestMapping(method = RequestMethod.GET, path = "/show")
 	@ResponseBody
@@ -42,14 +56,44 @@ public class ItemController {
 		return JSONFormatter.toJSON(result);
 	}
 
-	@RequestMapping(method = RequestMethod.POST, value = "/payment")
-	public @ResponseBody String createPayment(@RequestBody String payment) {
+	@RequestMapping(method = RequestMethod.POST, value = "/authorize")
+	public @ResponseBody String createAuthorize(@RequestBody String data) throws ClientProtocolException, IOException {
+		
+		String response = getResponse(url + "/authorize", data);
 
-		PdxInstance pdxInstance = JSONFormatter.fromJSON(payment);
+		PdxInstance pdxInstance = JSONFormatter.fromJSON(response);
 		
 		itemRegion.put(pdxInstance.getField("reference").toString(), pdxInstance);
 		
 		return "POST Done.";
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/capture")
+	public @ResponseBody String createCapture(@RequestBody String data) throws ClientProtocolException, IOException {
+		
+		String response = getResponse(url + "/capture", data);
+
+		PdxInstance pdxInstance = JSONFormatter.fromJSON(response);
+		
+		itemRegion.put(pdxInstance.getField("reference").toString(), pdxInstance);
+		
+		return "POST Done.";
+	}
+	
+	private String getResponse(String endpoint, String data) throws ClientProtocolException, IOException {
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		
+		StringEntity entity = new StringEntity(data);
+		HttpPost httppost = new HttpPost(endpoint);
+		httppost.setEntity(entity);
+		httppost.setHeader("content-type", "application/json");
+		
+		HttpResponse response = httpClient.execute(httppost);
+		HttpEntity responseEntity = response.getEntity();
+		
+		String content = EntityUtils.toString(responseEntity);
+		
+		return content;
 	}
 
 }
